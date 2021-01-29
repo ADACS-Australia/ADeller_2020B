64b75b0392b5bea638c6497279915093d9fc4cd1
Patrick Clearwater <pclearwater@swinburne.edu.au>
2021-01-29 11:04:59 +1100

    Preparing for GPU -- surrounding framework
    Add a separate process_gpu() function to 'Mode' which gets called if we are
    running the GPU version; as well as surrounding infrastructure to get it to
    compile in CUDA and non-CUDA environments.
    
    For now, the process_gpu() does the same thing as regular process(), but on the
    CPU (i.e. it's a straight copy of the code).
    
    Core::processdata() and associated data structures move from 'private' to
    'protected' so they can be override in the GPU base class (and 'execute' for
    now is no longer virtual).

--- mpifxcorr/src/gpucore.h
+++ mpifxcorr/src/gpucore.h
@@ -4,11 +4,19 @@
 #include "core.h"
 
 class GPUCore: public Core {
-	public:
-		GPUCore(const int id, Configuration *const conf, int *const dids, MPI_Comm rcomm)
-			: Core(id, conf, dids, rcomm) {};
+  public:
+    GPUCore(const int id, Configuration *const conf, int *const dids, MPI_Comm rcomm)
+      : Core(id, conf, dids, rcomm) {};
 
-		virtual void execute();
+    virtual void processdata(int index, int threadid, int startblock, int numblocks, Mode ** modes, Polyco * currentpolyco, threadscratchspace * scratchspace);
+
+  protected:
+    /*
+    virtual Mode *getMode(const int configindex, const int datastreamindex) {
+      return config->getMode(configindex, datastreamindex, true);
+    }
+    */
 };
 
 #endif
+// vim: shiftwidth=2:softtabstop=2:expandtab
